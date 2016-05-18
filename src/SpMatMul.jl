e6d2f388c11a54d86e72458ebf1c6f6c86bd6666
Eran Treister <erantreister@gmail.com>
2016-05-18 16:33:29 -0700

    ParSpMatVec tryout 2

--- src/SpMatMul.jl
+++ src/SpMatMul.jl
@@ -4,7 +4,7 @@ export SpMatMul,addVectors
 function SpMatMul(alpha::Union{Float64,Complex128,Complex64},AT::SparseCSCTypes,x::ArrayTypes,beta::Union{Float64,Complex128,Complex64},target::ArrayTypes,numCores::Int64)
 # function calculates: target = beta*target + alpha*A*x
 if hasParSpMatVec
-	Ac_mul_B!( alpha, A, x, beta, target, numCores );
+	ParSpMatVec.Ac_mul_B!( alpha, A, x, beta, target, numCores );
 else
 	Base.Ac_mul_B!(alpha,A,x,beta,target);
 end
@@ -17,7 +17,7 @@ function SpMatMul(AT::SparseCSCTypes,x::ArrayTypes,target::ArrayTypes,numCores::
 alpha = one(eltype(x));
 beta = zero(eltype(x));
 if hasParSpMatVec
-	Ac_mul_B!( alpha, A, x, beta, target, numCores );
+	ParSpMatVec.Ac_mul_B!( alpha, A, x, beta, target, numCores );
 else
 	Base.Ac_mul_B!(alpha,A,x,beta,target);
 end
